6aaaa564a12f20c51d9facee4234f6c609b6553b
preethi <selvampreethi360@gmail.com>
2024-12-13 19:57:38 +0530

    create project scaffold

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const Title = styled.div`
+  font-size: 1.5em;
+  font-family: var(--font-secondary);
+  text-align: center;
+  color: #BF4F74;
+`
+
+function App() {
+  return (
+    <Title>Your partner for an epic design system journey</Title>
+  );
+}
+
+export default App;
